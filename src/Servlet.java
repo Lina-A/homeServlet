25ad1bdbb5f3ce628f92b56f5a7a0953284c4a73
Lina-A <aeo6982@gmail.com>
2018-09-24 17:18:38 +0500

    delet some strange things

--- src/Servlet.java
+++ src/Servlet.java
@@ -15,8 +15,8 @@ public class Servlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
        
         String text =dadata.Sugg(request.getParameter("data")).toString();
-        response.setContentType("text/plain");  // Set content type of the response so that jQuery knows what it can expect.
-        response.setCharacterEncoding("UTF-8"); // You want world domination, huh?
+        response.setContentType("text/plain");  
+        response.setCharacterEncoding("UTF-8"); 
         response.getWriter().write(text);
     }
 
